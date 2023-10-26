0bcdb71bafdc4e15a5e3ba899262e87a2f24254d
Eduardo Quintana <edquintest@gmail.com>
2023-10-26 17:18:45 +0200

    Add unit and integration tests

---
+++ tests/MathHub.Api.Tests.Unit/MathServicesTests.cs
@@ -0,0 +1,53 @@
+﻿using FluentAssertions;
+using MathHub.Api.Services;
+using Xunit;
+
+namespace MathHub.Api.Tests.Unit;
+
+public class MathServicesTests
+{
+    private readonly MathService _mathService = new MathService();
+
+    [Fact]
+    public void IsEven_ShouldReturnTrue_WhenZeroIsProvided()
+    {
+        // Act
+        var isEven = _mathService.IsEven(0);
+
+        // Assert
+        isEven.Should().BeTrue();
+    }
+
+    [Theory]
+    [InlineData(2)]
+    [InlineData(4)]
+    [InlineData(16)]
+    [InlineData(10)]
+    [InlineData(100)]
+    [InlineData(5000)]
+    public void IsEven_ShouldReturnTrue_WhenEvenNumberIsProvided(int number)
+    {
+        // Act
+        var isEven = _mathService.IsEven(number);
+
+        // Assert
+        isEven.Should().BeTrue();
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(3)]
+    [InlineData(5)]
+    [InlineData(17)]
+    [InlineData(101)]
+    [InlineData(1001)]
+    [InlineData(50001)]
+    public void IsEven_ShouldReturnFalse_WhenOddNumberIsProvided(int number)
+    {
+        // Act
+        var isEven = _mathService.IsEven(number);
+
+        // Assert
+        isEven.Should().BeFalse();
+    }
+}
