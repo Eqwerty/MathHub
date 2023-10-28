bc92c061d753e5b5b3d6ec89ce3a22803c7bd775
Eduardo Quintana <edquintest@gmail.com>
2023-10-28 11:48:56 +0200

    Add failing test

--- tests/MathHub.Api.Tests.Unit/MathServicesTests.cs
+++ tests/MathHub.Api.Tests.Unit/MathServicesTests.cs
@@ -50,4 +50,10 @@ public class MathServicesTests
         // Assert
         isEven.Should().BeFalse();
     }
+
+    [Fact]
+    public void FailingTest()
+    {
+        true.Should().BeFalse();
+    }
 }
