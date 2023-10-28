8950ad145bd36bd3c1af88ae0e8080541e013427
Eduardo Quintana <edquintest@gmail.com>
2023-10-28 12:53:40 +0200

    Add hello world endpoint

--- src/MathHub.Api/Program.cs
+++ src/MathHub.Api/Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.MapGet("/", () => "Hello, world!");
 app.MapGet("/isEven/{number:int}", ([FromRoute] int number, [FromServices] IMathService mathService) => Results.Ok(mathService.IsEven(number)));
 
 app.UseSwagger();
