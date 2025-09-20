5101cb6279e89de6a604494093045d129304440c
Eduardo Quintana <eduardo.quintana@derivco.co.uk>
2025-09-20 19:45:09 +0200

    Add health checks and remove configuration endpoint

---
+++ src/MathHub.Api/HealthChecks/DatabaseHealthCheck.cs
@@ -0,0 +1,15 @@
+﻿using MathHub.Api.Options;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Options;
+
+namespace MathHub.Api.HealthChecks;
+
+internal class DatabaseHealthCheck(IOptions<DatabaseOptions> options) : IHealthCheck
+{
+    private readonly IOptions<DatabaseOptions> _options = options;
+
+    public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default) =>
+        Task.FromResult(_options.Value.ConnectionString is not null
+            ? HealthCheckResult.Healthy()
+            : HealthCheckResult.Unhealthy("Database connection string is not configured."));
+}
